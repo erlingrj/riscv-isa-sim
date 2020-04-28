78e6a7a56aecadd5908f3482c1318dc5420d5366
erling <erling.jellum@gmail.com>
2020-04-28 17:05:12 +0200

    fix up c++ linker errors

--- spike_main/spike-dasm.cc
+++ spike_main/spike-dasm.cc
@@ -25,7 +25,9 @@ int main(int argc, char** argv)
   parser.option(0, "isa", 1, [&](const char* s){isa = s;});
   parser.parse(argv);
 
-  processor_t p(isa, DEFAULT_VARCH, 0, 0);
+  struct ibda_params _ibda;
+
+  processor_t p(isa, DEFAULT_VARCH, 0, _ibda, 0);
   if (extension)
     p.register_extension(extension());
 
