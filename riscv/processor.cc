f75ac70e1ea7cd4aa29febe7d8f78e6ca0e80f68
erling <erling.jellum@gmail.com>
2020-04-30 12:25:20 +0200

    Fix up trace-dumping

--- riscv/processor.cc
+++ riscv/processor.cc
@@ -438,7 +438,7 @@ reg_t state_t::ist_get_tag(reg_t addr, reg_t bits) {
     instruction_pc[core_idx] = insn_pc;
     uint64_t bits = insn.bits() & ((1ULL << (8 * insn_length(insn.bits()))) - 1);
     instruction_bits[core_idx] = bits;
-    if (ibda_p.trace_level > 0) {
+    if (ibda_p.trace_level > 1) {
       fprintf(stderr, "0x%016" PRIx64 " (0xcd%08" PRIx64 ") core_idx:%d ibda:%d %s\n",
                        insn_pc, bits, core_idx, ibda[core_idx],p->disassembler->disassemble(insn).c_str());  
     }
